744cf8bc6cbe0f73b3e850654c5de1620ff1db3c
xiepeng <1321665847@qq.com>
2019-05-23 14:46:34 +0800

    初始提交

---
+++ src/test/java/com/xp/clothes/ClothesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xp.clothes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClothesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
